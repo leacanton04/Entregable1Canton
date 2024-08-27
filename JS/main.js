3b0363981d9f06571ca27a3ad8a0b57659a21f6a
Leandro Canton <leandro2canton@gmail.com>
2024-08-26 21:56:04 -0300

    puesta en funcionamiento de carrito y barra de busqueda

--- JS/main.js
+++ JS/main.js
@@ -1,151 +1,163 @@
-
-// Crear usuario
-class Usuario{
-    constructor(usuario, correo, contrasena){
-        this.usuario = usuario;
-        this.correo = correo;
-        this.contrasena = contrasena;
-    }
-    registrarUsuario(){
-        this.correo = prompt("Ingresa tu correo");
-        this.usuario = prompt("Crea tu usuario");
-        let contrasena = prompt("Crea tu contrasena");
-        let confirmContrasena = prompt("Confirma tu contrasena");
-        while(contrasena != confirmContrasena){
-            alert("Las contrasenas no coinciden");
-            contrasena = prompt("Crea tu contrasena");
-            confirmContrasena = prompt("Confirma tu contrasena");
-        }
-        this.contrasena = contrasena;
-        alert("Usuario registrado con exito");
-    }
-    iniciarSesion(){
-        let usuario = prompt("Ingresa tu usuario");
-        while(usuario != this.usuario){
-            alert("Usuario incorrecto, intente de nuevo");
-            usuario = prompt("Ingresa tu usuario");
-        }
-        let contrasena = prompt("Ingresa tu contrasena");
-        while(contrasena != this.contrasena){
-            alert("Contrasena incorrecta, intente de nuevo");
-            contrasena = prompt("Ingresa tu contrasena");
-        }
-        localStorage.getItem("usuario", JSON.stringify(this.usuario));
-    }
-    //si o si hay que crear un usuario, porque sino no hay nada guardado en los datos del Usuario. Si hubiera una base de datos, se podria hacer un login directamente
-}
-
 class Producto{
-    constructor(nombre, precio){
+    constructor(nombre, imgUrl, precio){
         this.nombre = nombre;
+        this.imgUrl = imgUrl;
         this.precio = parseFloat(precio);
     }
 }
 
 class Tienda{
     constructor(){
-        this.productos = [];
-        this.carrito = [];
+        this.productos = [
+            new Producto('Jordan 1 Chicago OG 1985', '../media/Moda/exclusivo1.png', 90000),
+            new Producto('Under Armour Curry 6 Splash Party', '../media/Moda/exclusivo2.png', 90000),
+            new Producto('Nike LeBron 17 Lakers ', '../media/Moda/exclusivo3.png', 90000),
+            new Producto('Adidas Dame 6 Ruthless', '../media/Moda/exclusivo4.png', 90000),
+            new Producto('Camiseta James Harden Huston Rockets', '../media/Moda/Remeras1.png', 100000),
+            new Producto('Camiseta Manu Ginobili San Antonio Spurs', '../media/Moda/Remeras2.png', 85000),
+            new Producto('Camiseta LeBron James Los Angeles Lakers', '../media/Moda/Remeras3.png', 100000),
+            new Producto('Camiseta Michel Jordan Chicago Bulls', '../media/Moda/Remeras4.png', 80000),
+            new Producto('Nike Kyrie 7 University Blue', '../media/Moda/calzado1.png', 90000),
+            new Producto('Tatum 2 Legacy', '../media/Moda/calzado2.png', 90000),
+            new Producto('Luka 2', '../media/Moda/calzado3.png', 90000),
+            new Producto('Harden Vol.8', '../media/Moda/oferta1.png', 63000) 
+            ];
+
+            this.carrito = [];
     }
 
-    agregarProductos(Producto){
-        this.productos.push({nombre: Producto.nombre, precio: Producto.precio});        
+    mostraarProductosPc(producto){
+
+        let articuloPc = document.createElement('article'); //creamos el aritculo para desktop
+        articuloPc.classList.add('conteiner', 'articulo-moda', 'conteiner-venta'); //agregamos las clases
+        
+        articuloPc.innerHTML = `
+            <img src="${producto.imgUrl}" alt="${producto.nombre}">
+            <p class="nombre-producto"> ${producto.nombre} </p>
+            <div>
+                <p class="precio"> $${producto.precio}</p>
+                <button class="add-to-cart" onclick="tienda1.agregarAlCarrito('${producto.nombre}')">
+                    <img src="../media/Iconos/icono-carrito.png" alt="Icono carrito">
+                </button>
+            </div>`;     
+               
+        return articuloPc;              
     }
 
-    mostrarProductos(){
-        for (const producto of this.productos) {
-            console.log(producto.nombre + " " + "$" + producto.precio);
-        }
+    mostraarProductosMobile(producto){
+        let articuloMobile = document.createElement('div'); //creamos el aritculo para desktop
+        articuloMobile.classList.add('carousel-item', 'active');//agregamos las clases
+
+        articuloMobile.innerHTML = `
+            <article class="conteiner articulo-moda conteiner-venta">
+                <img src="${producto.imgUrl}" alt="${producto.nombre}">
+                <p class="nombre-producto"> ${producto.nombre} </p>
+                <div>
+                    <p class="precio"> $${producto.precio}</p>
+                    <button class="add-to-cart" onclick="tienda1.agregarAlCarrito('${producto.nombre}')">
+                        <img src="../media/Iconos/icono-carrito.png" alt="Icono carrito">
+                    </button>
+                </div>
+            </article>`;
+
+        return articuloMobile;
+    }
+    
+
+    filtrarProductos(){
+        let inputBuscador = document.getElementById("buscar-input"); //recibe el input del buscador
+        let contenedorResultadosPc = document.getElementById("venta-pc"); //aca se muestran los productos en pc
+        let contenedorResultadosMobile = document.getElementById("venta-mobile"); //aca se muestran los productos en mobile
+        
+        this.productos.forEach(producto => {
+            contenedorResultadosPc.appendChild(this.mostraarProductosPc(producto));
+            contenedorResultadosMobile.appendChild(this.mostraarProductosMobile(producto));
+        });
+
+        inputBuscador.addEventListener("input", (e) => {
+            let cadena = inputBuscador.value;
+            console.log(cadena);
+            contenedorResultadosPc.innerHTML = "";
+            contenedorResultadosMobile.innerHTML = "";
+            
+            this.productos.forEach(producto => {
+                if (producto.nombre.toLowerCase().includes(cadena.toLowerCase())){
+                    console.log(producto.nombre);
+                    
+                    contenedorResultadosPc.appendChild(this.mostraarProductosPc(producto));
+                    contenedorResultadosMobile.appendChild(this.mostraarProductosMobile(producto));
+                }
+            }); 
+        });
+    } 
+    
+    actualizarNumeroCarrito(){
+        let numeroCarrito = document.getElementById("numero-carrito");
+        numeroCarrito.innerHTML = this.carrito.length;            
     }
 
-    agregarCarrito(nombreProducto){ 
-        for (const producto of this.productos) {
-            if (producto.nombre === nombreProducto) {
-                this.carrito.push(producto);
-            }
-        }
-        localStorage.getItem("carrito", JSON.stringify(this.carrito));
-    }
-
-    mostrarCarrito(){
-        for (const producto of this.carrito) {
-            console.log(producto.nombre + " " + "$" + producto.precio);
-        }
-    }
-
-    calcularPrecio(){
+    actualizarPrecioTotal(){
+        let precioTotal = document.getElementById("subtotal-precio");
         let total = 0;
-        for (const producto of this.carrito) {
+        this.carrito.forEach(producto => {
             total += producto.precio;
-        }
-        return(total);
+        });
+        precioTotal.innerHTML = `$ ${total}`;
+    }
+
+    agregarAlCarrito(productoNombre){
+        let producto = this.productos.find(p => p.nombre === productoNombre);
+        this.carrito.push(producto);
+        this.actualizarNumeroCarrito();
+        this.actualizarPrecioTotal();
+        this.mostrarCarrito();            
+    }
+    
+    mostrarCarrito(){
+        console.log(this.carrito);
+        let contenedorCarrito = document.getElementById("objetos-carrito");
+        let articuloCarrito = document.createElement('article'); 
+        articuloCarrito.classList.add('item-carrito'); 
+        
+        this.carrito.forEach(producto => {                
+            articuloCarrito.innerHTML = `
+                <img src="${producto.imgUrl}" alt="${producto.nombre}">
+                <div>
+                    <h2 class="nombre-producto">
+                            ${producto.nombre}
+                    </h2>
+                    <p class="pprecio">
+                        $${producto.precio}
+                    </p>
+                </div> `; 
+                contenedorCarrito.appendChild(articuloCarrito);
+                                
+        });            
+    }
+
+    vaciarCarrito(){
+        this.carrito = [];
+        this.actualizarNumeroCarrito();
+        this.actualizarPrecioTotal();
+        let contenedorCarrito = document.getElementById("objetos-carrito");
+        contenedorCarrito.innerHTML = "";
+        this.mostrarCarrito();
     }
 
     comprar(){
-        console.log("Usted va a comprar los siguientes productos:");
-        this.mostrarCarrito();
-        console.log("El total de su compra es de: $" + this.calcularPrecio());
-        this.carrito = [];
+        let respuesta = confirm("¿Desea confirmar la compra?");
+        if (respuesta == true){
+            alert("Gracias por su compra");
+            this.vaciarCarrito();           
+        }
     }
+    
+    
 }
 
-
-//REGISTRO E INICIO DE SESION
-
-if (localStorage.getItem("usuario") == null){
-    let inicioRegistro = confirm("¿Tienes usuario?");
-    if(inicioRegistro == true){
-        let usuario = new Usuario();
-        usuario.iniciarSesion();
-    }else{
-        let usuario = new Usuario();
-        usuario.registrarUsuario();
-        usuario.iniciarSesion();
-    }
-}else{
-    alert("Bienvenido de nuevo " + localStorage.getItem("usuario"));
-}
-
-
-//CARRITO
-
-//creamos un objeto tienda
 tienda1 = new Tienda();
-//creamos los objetos que va a tener en stock la tienda y los agregamos a la tienda
-tienda1.agregarProductos(new Producto("Jordan 1 Chicago OG 1985", 90000));
-tienda1.agregarProductos(new Producto("Under Armour Curry 6 'Splash Party'", 90000));
-tienda1.agregarProductos(new Producto("Nike LeBron 17 'Lakers'", 90000));
-tienda1.agregarProductos(new Producto("Adidas Dame 6 'Ruthless'", 90000));
-tienda1.agregarProductos(new Producto("Camiseta James Harden Huston Rockets", 100000));
-tienda1.agregarProductos(new Producto("Camiseta Manu Ginobili San Antonio Spurs", 85000));
-tienda1.agregarProductos(new Producto("Camiseta LeBron James Los Angeles Lakers", 100000));
-tienda1.agregarProductos(new Producto("Camiseta Michel Jordan Chicago Bulls", 80000));
-//mostramos los productos disponibles cargados previamente
-console.log("Productos disponibles:");
-tienda1.mostrarProductos();
-//selecionamos los productos que queremos agregar al carrito
-tienda1.agregarCarrito("Jordan 1 Chicago OG 1985");
-tienda1.agregarCarrito("Camiseta Michel Jordan Chicago Bulls");
-//mostramos el carrito
-console.log("\nProductos del carrito:");
-tienda1.mostrarCarrito();
-//usamos la funcion comprar
-console.log("\nComprando :)");
-tienda1.comprar();
-//corroboramos que el carrito este vacio
-console.log("\nProductos del carrito:");
-tienda1.mostrarCarrito();
-
-//todo esto lo hago para ir comprobando que todas las funciones esten funcionando correctamente
-
-
-
-
-
-
-
-
-
+tienda1.filtrarProductos();
+tienda1.actualizarNumeroCarrito();
 
 
 
